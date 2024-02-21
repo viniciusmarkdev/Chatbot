ee2af6db5541520b9d9bed6cb3de7ffa8bf44f4f
Marcos Vinicius <contato.marcoscandido@gmail.com>
2024-02-21 15:55:53 -0300

    rota-chat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ChatComponent } from './pages/chat/chat.component';
 
 export const routes: Routes = [
 
@@ -7,4 +8,13 @@ export const routes: Routes = [
         path:"",
         component: HomeComponent
     }
+    ,
+
+    {
+
+        path:"chat",
+        component:ChatComponent
+    }
+
+
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ArrowRightComponent } from '../../icon/arrow-right/arrow-right.component';
 import { SparkleComponent } from '../../sparkle/sparkle.component';
+import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -15,5 +17,13 @@ import { SparkleComponent } from '../../sparkle/sparkle.component';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  constructor(private router: Router){}
+
+
+  openChat(){
+          
+        this.router.navigate(["chat"])
+  }
 
 }
